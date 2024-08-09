f239457c8ea161f4997bb45231cbe81c853c38c4
cliveouma <cliveouma15@gmail.com>
2024-08-09 01:07:47 -0700

    Added env files for development and production also Update package.json to use cross-env for environment variable

--- backend/app.ts
+++ backend/app.ts
@@ -1,3 +1,12 @@
+import dotenv from 'dotenv';
+
+// Load common environment variables
+dotenv.config();
+
+// Load environment-specific variables
+const env = process.env.NODE_ENV || 'development';
+dotenv.config({ path: `.env.${env}` });
+
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
@@ -6,17 +15,13 @@ import authRoutes from './routes/authRoutes';
 import incidentRoutes from './routes/incidentRoutes';
 import cookieParser from 'cookie-parser';
 import adminRoutes from './routes/adminRoutes';
-import dotenv from 'dotenv';
-
-// Load environment variables
-dotenv.config();
 
 const app = express();
 
 // Middleware
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true // Allows cookies to be sent with requests
 }));
--- backend/dist/app.js
+++ backend/dist/app.js
@@ -3,6 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+// Load common environment variables
+dotenv_1.default.config();
+// Load environment-specific variables
+const env = process.env.NODE_ENV || 'development';
+dotenv_1.default.config({ path: `.env.${env}` });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const body_parser_1 = __importDefault(require("body-parser"));
@@ -11,14 +17,11 @@ const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
 const incidentRoutes_1 = __importDefault(require("./routes/incidentRoutes"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const adminRoutes_1 = __importDefault(require("./routes/adminRoutes"));
-const dotenv_1 = __importDefault(require("dotenv"));
-// Load environment variables
-dotenv_1.default.config();
 const app = (0, express_1.default)();
 // Middleware
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true // Allows cookies to be sent with requests
 }));
